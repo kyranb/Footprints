3cce530b4e7dfb0f490bf3a647b9fdf6260148c6
Anders Bilfeldt <abilfeldt@gmail.com>
2021-03-01 20:57:49 +0100

    Move migration stub file to "default" folder/naming structure and implement proper database setup to phpunit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -25,6 +25,15 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         ];
     }
 
+    public function getEnvironmentSetUp($app)
+    {
+        // import the CreatePostsTable class from the migration
+        include_once __DIR__ . '/../database/migrations/create_footprints_table.php.stub';
+
+        // run the up() method of that migration class
+        (new \CreateFootprintsTable)->up();
+    }
+
     /**
      * Define routes setup.
      *
